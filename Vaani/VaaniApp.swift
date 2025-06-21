32752f8b330eb7009b7462af73478095544c7623
pravalgautam <71179696+pravalgautam@users.noreply.github.com>
2025-06-22 00:30:19 +0530

    Initial Commit

---
+++ Vaani/VaaniApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaaniApp.swift
+//  Vaani
+//
+//  Created by Praval Gautam on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct VaaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
